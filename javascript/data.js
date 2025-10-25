791f673cb4c2df7a42abc7124737f760045d583e
Umema Furqan <umemafurqan979@gmail.com>
2025-10-25 01:42:35 -0700

    first commit

---
+++ javascript/data.js
@@ -0,0 +1,78 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "Sleek & Shine Shampoo",
+    "price": 10000,
+    "description": "Garnier is committed to continually improving the impact of its products throughout the product lifecycle, including the production and anticipated usage. This tool is designed to help assess the relative environmental & social impact of US Garnier Products compared to other L’Oreal Groupe products in the same category around the world in 2020.",
+    "brand": "Garnier ",
+    "image": "https://www.garnierusa.com/-/media/project/loreal/brand-sites/garnier/usa/us/products/fructis/sleek-and-shine/2023/sleek-shine-shampoo-12-5oz/front-pack-shot-jpg.jpg?w=450&rev=bd8a35fe68554e259c898e45dd85a6d1&hash=AEF7957CC98B7BEAE4163E301065C52F"
+}, {
+    "id": 2,
+    "title": "SHAMPOO HERBAL STRONG",
+    "price": 5000,
+    "description": "Lifebuoy Herbal Strong Shampoo is a testament to the fusion of nature’s goodness and advanced hair care technology. Crafted meticulously with a blend of powerful herbal ingredients, this shampoo is a sanctuary for your hair.",
+    "brand": "LIFEBUOY",
+    "image": "https://www.alfatah.pk/cdn/shop/products/8961014246926_1024x1024.jpg?v=1639553784"
+}, {
+    "id": 3,
+    "title": "Black Shine Conditioner",
+    "price": 69999,
+    "description": "Water, Cetaryl alcohol, Dimethicone, Amodimethicone, Peg7 Propylhelptylether, Cetrimonium chloride, Behentrimonium chloride, Perfume, Steramidopropyl dimethlyamine, Disodium EDTA, Lactic acid, lysin HCL, Penthenol,",
+    "brand": "Sunsilk",
+    "image": "https://assets.unileversolutions.com/v1/87155167.png?im=AspectCrop=(540,540);Resize=(540,540)"
+}, {
+    "id": 4,
+    "title": "Honey  Repairing Conditioner",
+    "price": 36999,
+    "description": "If your hair is suffering from split ends, this nourishing conditioner is for you! This unique blend of sustainably sourced Acacia honey and beeswax for hair instantly repairs damaged hair by fighting against hair breakage.Treasures repairing shampoo; 9/10 consumers are immediately satisfied!",
+    "brand": "Garnier",
+    "image": "https://www.garnierusa.com/-/media/project/loreal/brand-sites/garnier/usa/us/products/whole-blends/honey-treasures/conditioner/honeytreasures_conditioner_125oz/garnier_wholeblends_honeytreasure_conditioner_125oz_750x750_1.jpg?w=450&rev=be7f13040c444b378dbacb2a55b5a9e4&hash=575122918263CC41A3BE07ED7E387999"
+}, {
+    "id": 5,
+    "title": "Hair Dryer",
+    "price": 35499,
+    "description": "The uniquely-designed ThermoProtect attachment powerfully mixes warm and cool air for everyday care. It drops the temperature by 15°C while still drying your hair quickly.",
+    "brand": "Philips",
+    "image": "https://images.philips.com/is/image/philipsconsumer/ac5c255b3f054f6c9ad5ae7d00c407ff?wid=420&hei=360&$jpglarge$"
+}, {
+    "id": 6,
+    "title": "Dawlance",
+    "price": 179999,
+    "description": "Equipped with multiple heat and speed settings, the Dawlance Hair Dryer allows you to customize your drying experience according to your hair type and styling needs.",
+    "brand": "Dawlance",
+    "image": "https://www.dawlance.com.pk/media/resize/F01000000307_LO1_20230725_140310.png/1000Wx1000H/image.webp"
+}, {
+    "id": 7,
+    "title": "Hair straighteners",
+    "price": 18000,
+    "description": "Hair straightener for professional styling - the Triple Ionic Function with 3 stages provides individual styling and shiny hair. ",
+    "brand": "Beurer",
+    "image": "https://beurer.pk/assets/p_img/hs_80-glaetteisen-beurer.jpg"
+}, {
+    "id": 8,
+    "title": "TITANIUM BRILLIANCE STRAIGHT",
+    "price": 32999,
+    "description": "Effortless straightening and easy curls or waves with our rose gold Titanium Brilliance Straight & Curl.",
+    "brand": "babyliss",
+    "image": "https://www.babyliss.com/dw/image/v2/BDGM_PRD/on/demandware.static/-/Sites-master-uk/default/dw9f6ee224/BaByliss-ML/Haircare/Straighteners/2481U/BAB_2481U_BaByliss%20Straightener_2.jpg?sw=800&sh=900&sm=fit&strip=false"
+}, {
+    "id": 9,
+    "title": "Spray Bottle",
+    "price": 1000,
+    "description": "This amazing Japanese Sprayer 250ml bottle spray’s 50% longer than any other spray bottle on the market. Long Lifetime Soft Spraying Durable Material",
+    "brand": "Eagle Fortress",
+    "image": "https://www.salondziners.com/wp-content/uploads/2023/02/13260360.png"
+}, {
+    "id": 10,
+    "title": "Mozza Hair Spray Bottle",
+    "price": 500,
+    "description": "Sprays like an aerosol without actually being an aerosol. It contains no propellants of any kind. Leaves no wet spots and sprays without dribbling or drenching.",
+    "brand": "Mozza",
+    "image": "https://i5.walmartimages.com/seo/2-Pack-Mozza-Hair-Spray-Bottle-Continuous-Mist-Trigger-Sprayer-for-Hairstyling-Cleaning-Plants-Misting-Skin-Care_f893bffb-1c28-4d4a-aac2-a62a975cab04.b156e2983c6cecbec6e01028b08468d5.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF"
+}]
